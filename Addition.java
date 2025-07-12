cbe624543cd247db8360200c5c527dd447b3c29a
Ketan narhe <ketannarhe02@gmail.com>
2025-07-12 16:30:58 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package com.NumberPro;
+
+public class Addition {
+
+	public static void main(String[] args) 
+	{
+	   int a=20;
+	   int b=30;
+	   
+	   int ans=a+b;
+	   
+	   System.out.println(ans);
+		
+
+	}
+
+}
