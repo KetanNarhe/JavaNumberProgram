b9af8d341767ea2c5fef0eb710ed4669560e369f
Ketan narhe <ketannarhe02@gmail.com>
2025-07-12 16:33:31 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+package com.NumberPro;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+         int num=7;
+         
+         if(num%2==0)
+         {
+        	 System.out.println("It is a Even Number");
+         }
+         else
+         {
+        	 System.out.println("It is Odd Number");
+         }
+
+	}
+
+}
