10164f3611b65fd3e90978ef513de326cedc2d8f
Ketan narhe <ketannarhe02@gmail.com>
2025-09-10 23:23:56 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,34 @@
+package BasicNumberProg;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter Any value");
+		int num=sc.nextInt();
+		
+		int sum=0;
+		
+		for(int i=1;i<num;i++)
+		{
+			if(num%i==0)
+			{
+				sum+=i;
+			}
+		}
+		if(num==sum)
+		{
+			System.out.println("Perfect Number");
+		}
+		else
+		{
+			System.out.println("Not perfect ");
+		}
+
+	}
+
+}
